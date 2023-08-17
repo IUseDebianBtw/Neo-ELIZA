d7b6f9b94d6ada14504c60c51d49efa7ac2512b2
bossman7309 <bossman7309d@gmail.com>
2023-08-16 23:41:01 -0500

    fixes2

--- src/main.py
+++ src/main.py
@@ -3,27 +3,36 @@ import sys
 
 print(open('src/ascii_art.txt').read())
 
-openai.api_key = input("Please enter your OpenAI API key: ")
+def get_api_key():
+    openai.api_key = input("Please enter your OpenAI API key (or type 'q'/'quit' to exit): ")
 
-def neo_eliza():
-    print("Neo-Eliza: Hello! I'm Neo-Eliza. Type 'exit' to end the chat.")
-    
+    # Allow user to exit without entering an API key
+    if openai.api_key.lower() in ['q', 'quit']:
+        sys.exit(0)
+
+def neo_eliza():   
     while True:
         user_input = input("You: ")
         
         # Allow the user to exit the chat
         if user_input.lower() in ['exit', 'quit']:
-            print("Neo-Eliza: Goodbye!")
             sys.exit(0)
         
-        # Fetching the response from ChatGPT
-        response = openai.Completion.create(
-            model="text-davinci-002",  # Choose the model you prefer
-            prompt=user_input,
-            max_tokens=150  # You can adjust this value as needed
-        )
-        
-        print(f"Neo-Eliza: {response.choices[0].text.strip()}")
+        try:
+            # Fetching the response from ChatGPT
+            response = openai.Completion.create(
+                model="gpt-4",
+                prompt=user_input,
+                max_tokens=32000 
+            )
+            print(f"Neo-Eliza: {response.choices[0].text.strip()}")
+        except openai.error.OpenAIError as e:
+            if 'authentication' in str(e).lower():
+                print("Error: Invalid API key.")
+                get_api_key()
+            else:
+                print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
+    get_api_key()  # Prompt user for API key initially
     neo_eliza()
