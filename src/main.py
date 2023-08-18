7454d5648193e2754d5128e8e664740e26ae9630
Bossman7309 <85088259+IUseDebianBtw@users.noreply.github.com>
2023-08-17 23:23:56 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,22 +4,34 @@ import sys
 print(open('src/ascii_art.txt').read())
 
 def get_api_key():
-    openai.api_key = input("Please enter your OpenAI API key (or type 'q'/'quit' to exit): ")
+    while True:  
+        openai.api_key = input("Please enter your OpenAI API key (or type 'q'/'quit' to exit): ")
 
-    # Allow user to exit without entering an API key
-    if openai.api_key.lower() in ['q', 'quit']:
-        sys.exit(0)
+        if openai.api_key.lower() in ['q', 'quit']:
+            sys.exit(0)
+        
+        try:
+            response = openai.Completion.create(
+                model="gpt-4",
+                prompt="test",
+                max_tokens=5
+            )
+            break  
+        except openai.error.OpenAIError as e:
+            if 'authentication' in str(e).lower():
+                print("Error: Invalid API key. Please enter a valid key.")
+            else:
+                print(f"An error occurred: {e}")
+
 
 def neo_eliza():   
     while True:
         user_input = input("You: ")
         
-        # Allow the user to exit the chat
         if user_input.lower() in ['exit', 'quit']:
             sys.exit(0)
         
         try:
-            # Fetching the response from ChatGPT
             response = openai.Completion.create(
                 model="gpt-4",
                 prompt=user_input,
@@ -34,5 +46,5 @@ def neo_eliza():
                 print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    get_api_key()  # Prompt user for API key initially
+    get_api_key()  
     neo_eliza()
