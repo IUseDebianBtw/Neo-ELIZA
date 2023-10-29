626a9c5049cf5ca92b5a9f62aa86388e8af772fa
Netrunner <85088259+IUseDebianBtw@users.noreply.github.com>
2023-10-29 17:01:48 -0500

    Update main.py

--- neo_eliza/main.py
+++ neo_eliza/main.py
@@ -1,8 +1,20 @@
-from .neo_eliza import neo_eliza
-from .get_api_key import get_api_key
+import pwd
+import os
+import subprocess
+from neo_eliza import neo_eliza
+from get_api_key import get_api_key
 
-print(open('neo_eliza/ascii_art.txt').read())
+def main():
+    try:
+        print(open('ascii_art.txt').read())
+        
+        get_api_key()
+        neo_eliza()
+
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Exiting gracefully.")
+        exit(0)
+
 
 if __name__ == "__main__":
-    get_api_key()
-    neo_eliza()
+    main()
